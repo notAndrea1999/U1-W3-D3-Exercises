dacf65eee2ceb8474b8515651d656b40439e075b
Andrea De Masi <andreademasi1999@gmail.com>
2023-10-18 16:34:46 +0200

    cleaning the code

--- src/main/java/andreademasi/Application.java
+++ src/main/java/andreademasi/Application.java
@@ -22,11 +22,6 @@ public class Application {
         Person p3 = new Person("Gianni", "Bruno", "giannib@sobello.it", d1, Gender.M);
         Person p4 = new Person("Marco", "Avaria", "marcom@sobello.it", d1, Gender.M);
         Person p5 = new Person("Adelina", "Ionio", "adelinai@sobello.it", d1, Gender.F);
-//        pd.save(p1);
-//        pd.save(p2);
-//        pd.save(p3);
-//        pd.save(p4);
-//        pd.save(p5);
 
 
         LocationDAO ld = new LocationDAO(em);
@@ -35,11 +30,7 @@ public class Application {
         Location l3 = new Location("Location 3", "Napoli");
         Location l4 = new Location("Location 4", "Pesaro");
         Location l5 = new Location("Location 5", "Gerusalemme");
-//        ld.save(l1);
-//        ld.save(l2);
-//        ld.save(l3);
-//        ld.save(l4);
-//        ld.save(l5);
+
 
         EventoDAO ed = new EventoDAO(em);
         LocalDate d2 = LocalDate.of(2023, 01, 01);
@@ -49,18 +40,6 @@ public class Application {
         // Evento e4 = new Evento("Dance4", d2, "Una bella Descrizione", tipoEvento.randomTipoEvento(), 200);
         // Evento e5 = new Evento("Dance5", d2, "Una bella Descrizione", tipoEvento.randomTipoEvento(), 200);
 
-        Location foundLocation = ld.findById(1);
-        Location foundLocation2 = ld.findById(4);
-
-//        e1.setLocation(foundLocation2);
-//        ed.save(e1);
-//        ed.findByIdAndDelete(23);
-
-//        ed.save(e1);
-//        ed.save(e2);
-//        ed.save(e3);
-//        ed.save(e4);
-//        ed.save(e5);
 
         PartecipationDAO partd = new PartecipationDAO(em);
         Person foundPerson = pd.findById(1);
